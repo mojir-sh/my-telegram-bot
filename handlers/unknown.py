3861264061637209b899276cc61a75f6ada89677
mojir-sh <mojirshatryan0@gmail.com>
2026-08-18 12:23:18 +0330

    Create unknown.py

---
+++ handlers/unknown.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from services.notify import notify_owner
+
+
+async def unknown_message(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> None:
+
+    user = update.effective_user
+
+    if user.id == 8898410167:
+        return
+
+    await update.message.reply_text(
+        "اگر یه بار دیگه این کارو بکنی، "
+        "اسم‌ت رو می‌دم صاحبم بیاد بالا سرت 😎"
+    )
+
+    await notify_owner(
+        context,
+        user,
+        "پیام غیرمجاز ارسال کرد"
+    )
