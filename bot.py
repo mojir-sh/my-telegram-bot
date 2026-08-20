9d6b32ff2d3b43447074add4a0ceddd88afe465b
mojir-sh <mojirshatryan0@gmail.com>
2026-08-20 11:33:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2071,6 +2071,11 @@ def main():
     app.add_handler(CallbackQueryHandler(comment_callback, pattern="^comment:"))
     app.add_handler(MessageHandler(filters.TEXT, receive_comment), group=1)
 
+app.add_handler(MessageHandler(
+    filters.PHOTO | filters.VIDEO | filters.Document.ALL | filters.ANIMATION | filters.AUDIO,
+    broadcast_media
+), group=2)
+
     # تشخیص لفت دادن از کانال
     app.add_handler(ChatMemberHandler(on_chat_member_update, ChatMemberHandler.CHAT_MEMBER))
 
